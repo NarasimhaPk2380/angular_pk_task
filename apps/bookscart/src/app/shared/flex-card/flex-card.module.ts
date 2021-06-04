4372b80aaa1245361c946189007d530a1a3ae01e
durga prasad <nprasad@nprasad-lp.pkrmsezdc.com>
2021-06-04 08:07:07 +0530

    Flexlayout module added

--- apps/bookscart/src/app/shared/flex-card/flex-card.module.ts
+++ apps/bookscart/src/app/shared/flex-card/flex-card.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@a2zcart/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [CardComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, FlexLayoutModule],
   exports: [CardComponent],
 })
 export class FlexCardModule {}
